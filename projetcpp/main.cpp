3f15addc0afc572ebd839cf7078c708cad8d5ad3
laura-gen <laura33.geneau@gmail.com>
2025-12-26 15:02:06 +0100

    is_hit de cube

---
+++ projetcpp/main.cpp
@@ -0,0 +1,114 @@
+
+// qui affiche un dégradé : 
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include "rayon.h"
+
+int main(void) {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    //taille de l'écran
+    int y_taille = 640;
+    int x_taille = y_taille;
+    SDL_Window* window = SDL_CreateWindow("Ray Tracing MVP0.1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, x_taille, y_taille, 0);
+    // fenetre , SDL_WINDOWPOS_CENTERED 2 fois pour dire la position x et y où je veux que ma fenêtre soit, taille de la fenêtre large
+    // puis haut, et 0 pour 0 options supplémentaires
+    // Création du renderer pour pouvoir dessiner des points
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    // renderer = pinceau , -1 c'est un truc par défaut, c'est une sorte d'outil utilisé pour dessiner, 
+    //genre pinceau ou crayon, et SDL_RENDERER_ACCELERATED c'est pour utiliser le GPU au lieu du CPU
+    //en fait c'est pour aller plus vite
+
+
+    // avant while 
+    Camera cam(Vector3f(0,0,-100),Vector3f());
+
+    Material rouge(255, 0, 0, 0);
+    Sphere s(Vector3f(0.2f, 0.3f, 1.0f), 0.5f, rouge); 
+
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) running = false;
+        }
+        //je note tout ce qui se passe dans la fenetre sdl
+        // si jamais il se passe un truc de type quit c est à dire la croix et bien on quitte
+
+
+        // --- DEBUT DU DESSIN ---
+        for (int y = 0; y < y_taille; y++) {
+            for (int x = 0; x < x_taille; x++) {
+                // Ici, je remplace plus tard par le calcul du rayon
+                // On transforme x et y (0 à 640) en coordonnées centrées (-1.0 à 1.0)
+                // pour que le centre de l'écran soit (0,0)
+                //float coord_x = (x - x_taille/2) / 320.0f;
+                //float coord_y = (y - y_taille/2) / 240.0f;
+                float coord_x = (x - x_taille / 2.0f) / (x_taille / 2.0f);
+                float coord_y = (y - y_taille / 2.0f) / (y_taille / 2.0f);
+
+/*
+                int lignes = 400;
+                int colonnes = 350;
+
+                stv::vector<std::vector<pair>> v(lignes, std::vector<pair>(colonnes, 0.0f));
+                for (int x = 0; x < x_taille; x++) {
+                        v[x][0].first = x_taille/2;
+                    for (int y = 1; y < y_taille; y++) {
+                        v[x][y] = v[x][y-1] +1;
+                
+
+                for (int y = 0; y < y_taille; y++){
+                    v[0][y].second = y_taille/2;
+                    for (int x = 0; x < x_taille; x++) {
+                        v[y][x] = v[y][x-1] +1;
+
+                    }
+
+                }        
+*/                        
+
+                // Création du rayon : 
+                
+                //Ray3f ray(cam.position_, Vector3f(coord_x, coord_y, 0));
+
+                // La direction est : PointSurEcran - PositionCaméra
+                Vector3f direction = Vector3f(coord_x, coord_y, 0) - cam.position_;
+                Ray3f ray(cam.position_, direction);
+
+                // ça ça va changer, c'est à dire que le rayon va être projeté de la caméra ves le pixel.
+                // donc l'origine sera bien (x,y,-50) par exemple
+                // comme ça on ne change pas l'endroit 
+                //bon en fait là je place la caméra en origine x y -10
+                //bref c'est le 2.a ici
+                
+
+                // je veux en x en y et en z en fonction de sphere
+                if (s.is_hit(ray)) {
+                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                } else {
+                    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                }
+
+                //SDL_SetRenderDrawColor(renderer, (x % 255), (y % 255), 150, 255);
+                //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                //SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+                // je veux que l'outil renderer = pinceau change de couleur
+                // pour Rouge Vert Bleu et transparence
+                SDL_RenderDrawPoint(renderer, x, y);
+                // je veux que le pinceau se pose en x y
+            }
+        }
+        SDL_RenderPresent(renderer); 
+        // montrer le dessin ( il était caché)
+
+        // --- FIN DU DESSIN ---
+    }
+
+    SDL_DestroyRenderer(renderer); // free
+    SDL_DestroyWindow(window); // free
+    SDL_Quit(); 
+    return 0;
+}
+
+    
---
+++ projetcpp/rayon.h
@@ -0,0 +1,334 @@
+#ifndef RAYON_H
+#define RAYON_H
+#include <iostream>
+#include <cmath>
+//ajout d'un include
+#include <vector>
+
+class Vector3f {
+    public :
+        float x_; 
+        float y_;
+        float z_; 
+        Vector3f() {
+            x_= 0; 
+            y_ = 0;
+            z_ = 0;
+        };
+        Vector3f(float x, float y, float z){
+            x_= x; 
+            y_ = y;
+            z_ = z;   
+        }
+        Vector3f operator+ (Vector3f v1) const {  // const car on ne veut pas modifier le vecteur; & ? vect3f n'est pas une grosse struct 
+            Vector3f v;
+            v.x_ =  x_ + v1.x_;  //ATENTION A L'ORDRE
+            v.y_ = y_ + v1.y_;
+            v.z_ =  z_ + v1.z_;
+            return v;
+        }
+        Vector3f operator- (Vector3f v1) const {  // const car on ne veut pas modifier le vecteur; & ? vect3f n'est pas une grosse struct 
+            Vector3f v;
+            v.x_ =  x_ - v1.x_;  //ATENTION A L'ORDRE
+            v.y_ = y_ - v1.y_;
+            v.z_ =  z_ - v1.z_;
+            return v;
+        }
+
+
+        Vector3f operator* (float a) const {  // const car on ne veut pas modifier le vecteur; & ? vect3f n'est pas une grosse struct 
+            Vector3f v;
+            v.x_ = a * x_;
+            v.y_ = a * y_;
+            v.z_ = a * z_;
+            return v;
+        }
+
+        Vector3f operator* (Vector3f v1) const {  // const car on ne veut pas modifier le vecteur; & ? vect3f n'est pas une grosse struct 
+            Vector3f v;
+            v.x_ = v1.x_ * x_;  // multiplaction commutative 
+            v.y_ = v1.y_ * y_;
+            v.z_ = v1.z_ * z_;
+            return v;
+        }
+
+        Vector3f directionV (Vector3f v) const{
+            Vector3f d;
+            d.x_ = v.x_ - x_;
+            d.y_ = v.y_ - y_;
+            d.z_ = v.z_ - z_;
+            return d;
+
+        }
+
+        float norme () const{  //normalisation 
+            float n;
+            n = std::sqrt(x_ * x_ + y_ * y_ + z_ * z_);
+            return n; 
+
+        }
+
+        Vector3f normalise() const{ //normalise un vecteur 
+            float n = norme();
+            Vector3f v = Vector3f(0, 0, 0);
+            if (n>0){
+                float m = 1.0f / n;
+                v.x_= x_ * m;
+                v.y_ = y_ * m;
+                v.z_ = z_* m;
+            }
+            return v;
+        }
+};
+
+float prod_scal (Vector3f v1, Vector3f v2);
+Vector3f prod_vect(Vector3f v1, Vector3f v2);
+Vector3f direction (Vector3f v, Vector3f w);
+bool egal (Vector3f v1, Vector3f v2);
+
+class Ray3f {
+    public: 
+        Vector3f origin_; 
+        Vector3f direction_;
+        Ray3f(){
+            origin_ = Vector3f();
+            direction_ = Vector3f();
+        }
+        Ray3f(Vector3f o, Vector3f d){
+            origin_ = o;
+            direction_ = d;
+        }
+};
+
+class Camera {
+    public: 
+        Vector3f position_; 
+        Vector3f direction_;
+        Camera(){
+            position_ = Vector3f();
+            direction_ = Vector3f();  
+        }
+
+        Camera(Vector3f p, Vector3f d){
+            position_ = p;
+            direction_ = d;
+        }
+
+};
+
+class Material {
+    public: 
+        float r_;
+        float g_;
+        float b_; 
+        float shininess_;
+
+        Material(){
+            r_ = 0;
+            g_ = 0;
+            b_ = 0;
+            shininess_ =0;
+        }
+        Material(float r, float g, float b, float s){
+            r_ = r;
+            g_ = g;
+            b_ = b;
+            shininess_ =s;
+        }
+
+};
+
+class Shape {
+    public: 
+        Material matter_; 
+
+        virtual bool is_hit(Ray3f ray) =0 ;  // & ? pas grosse struct 
+        virtual Ray3f reflect(Ray3f ray)=0;
+
+
+};
+
+
+
+
+class Cube : public Shape{
+    public: 
+        Vector3f origin_; 
+        Vector3f height_;
+        Vector3f width_; // pour faire des paralélépipèdes
+        Cube(){
+            origin_ = Vector3f();
+            height_ = Vector3f();
+            width_ = Vector3f();
+            matter_ = Material();
+        }
+
+        Cube(Vector3f o, Vector3f h,Vector3f w, Material m){
+            origin_ = o;
+            height_ = h;
+            width_ = w;
+            matter_ = m;
+        }
+
+        bool is_hit(Ray3f ray) const;
+
+        bool est_dans_surf(Vector3f w,  Vector3f h, Vector3f d, Vector3f direction, Vector3f v) const{
+            Vector3f depth = prod_vect(width_,height_);
+
+            float dist_u = 0;
+            float dist_v = 0;
+            float max_u = 0;
+            float max_v = 0;
+
+            if (egal(direction,width_) || egal(direction,width_ * -1)) {
+                // Face Haut ou Bas
+                dist_u = prod_scal(v, w);
+                dist_v = prod_scal(v, d);
+                max_u = width_.norme() * 0.5f;
+                max_v = depth.norme() * 0.5f;
+            } 
+            else if (egal(direction,height_) || egal(direction,height_ * -1)) {
+                // Face Droite ou Gauche 
+                dist_u = prod_scal(v, h);
+                dist_v = prod_scal(v, d);
+                max_u = height_.norme() * 0.5f;
+                max_v = depth.norme() * 0.5f;
+            } 
+            else {
+                // Face Devant ou Derrière 
+                dist_u = prod_scal(v, w);
+                dist_v = prod_scal(v, h);
+                max_u = width_.norme() * 0.5f;
+                max_v = height_.norme() * 0.5f;
+            }
+            return (((std::abs(dist_u) <= max_u) && (std::abs(dist_v) <= max_v))); // Le point est à l'intérieur 
+
+}
+
+
+
+/* 
+            if (egal (direction,width_)){
+            float dist_u = prod_scal(direction,depth );
+            float dist_v = prod_scal(direction, height_);
+            }
+                else {
+                    if (egal(direction, width_ * (-1))){
+                    float dist_u = prod_scal(direction,depth* (-1) );
+                    float dist_v= prod_scal(direction, height_* (-1));
+                    }
+                    else {
+                        if (egal(direction,height_)){
+                        float dist_u = prod_scal(direction,depth);
+                        float dist_v = prod_scal(direction, width_);
+                        }
+                        else{
+                            if (egal(direction,height_ * (-1))){
+                            float dist_u = prod_scal(direction,depth* (-1) );
+                            float dist_v = prod_scal(direction, width_* (-1));
+                            }
+                            else{
+                                if (egal(direction,depth)){
+                                float dist_u = prod_scal(direction,width_ );
+                                float dist_v= prod_scal(direction, height_);
+                                }
+                                else{
+                                    if (egal(direction, width_ * (-1))){
+                                    float dist_u = prod_scal(direction,width_* (-1) );
+                                    float dist_v = prod_scal(direction, height_* (-1));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+        }
+
+*/
+
+
+/*
+        std::vector<Vector3f> vect_normal(){
+            std::vector<Vector3f> v;
+
+            Vector3f depth = prod_vect(width_, height_);
+            
+            // face haut 
+            
+
+
+            // face devant 
+            
+            
+            //face coté 
+            
+
+            return v;
+        }
+*/
+        bool is_hit(Ray3f ray);
+        Ray3f reflect(Ray3f ray);
+
+};
+
+class Quad : public Shape{
+    public: 
+        Vector3f origin_; 
+        Vector3f width_;
+
+        Vector3f height_;
+        Quad(){
+            origin_ = Vector3f();
+            width_= Vector3f();
+            height_ = Vector3f();
+            matter_ = Material();
+        }
+
+        Quad(Vector3f o, Vector3f w, Vector3f h, Material m){
+            origin_ = o;
+            width_= w;
+            height_ = h;
+            matter_ = m;
+        }
+
+
+        bool is_hit(Ray3f ray);
+        Ray3f reflect(Ray3f ray);
+};
+
+class Sphere : public Shape{
+    public: 
+        Vector3f origin_; 
+        float radius_;
+        
+        Sphere(){
+            origin_ = Vector3f();
+            radius_= 0;
+            matter_ = Material();
+        }
+
+        Sphere(Vector3f o, float r, Material m){
+            origin_ = o;
+            radius_= r;
+            matter_ = m;
+        }
+        bool is_hit(Ray3f ray);
+        Ray3f reflect(Ray3f ray);
+};
+
+
+class Scene {
+    public: 
+        Camera camera_; 
+        std::vector<Shape*> shapes_;
+        Ray3f source_;
+        void render(int width, int height, std::string filename);
+
+};
+
+// class Grille {
+//     int width_;
+//     int height_; 
+// };
+
+#endif
